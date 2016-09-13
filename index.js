d59f4831b59e23b980f6d608c851544f6922e4f7
DaniVarga <daniel.varga@dff-solutions.de>
2016-09-13 18:43:40 +0200

    window.jQuery

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 if (require){
-    try{
-        if (window && !window.moment) {
-            window.moment = require('moment');
-        }
+    if (window && !window.moment) {
+        window.moment = require('moment');
+    }
 
-        if (window && !window._) {
-            window._ = require('lodash');
-        }
+    if (window && !window._) {
+        window._ = require('lodash');
+    }
 
-        if (window && !window.$ && !window.jQuery) {
-            window.$ = window.jQuery = require('jquery');
-        }
+    if (window && !window.$ && !window.jQuery) {
+        window.$ = require('jquery');
+        window.jQuery = window.$
+    }
 
-        if (typeof $().emulateTransitionEnd != 'function') {
-            require('bootstrap-sass');
-        }
+    if (typeof $().emulateTransitionEnd != 'function') {
+        require('bootstrap-sass');
+    }
 
-        if (typeof $().daterangepicker != 'function') {
-            require('bootstrap-daterangepicker');
-        }
-    } catch (e) {
-        //TODO
+    if (typeof $().daterangepicker != 'function') {
+        require('bootstrap-daterangepicker');
     }
 }
 require('./dist/js/angular-bsdaterangepicker.js');
