ee109639318f1d5c4db30ff8eda5248840117802
DaniVarga <daniel.varga@dff-solutions.de>
2016-09-13 18:28:16 +0200

    optimized imports

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
+if (require){
+    try{
+        if (window && !window.moment) {
+            window.moment = require('moment');
+        }
+
+        if (window && !window._) {
+            window._ = require('lodash');
+        }
+
+        if (window && !window.$ && !window.jQuery) {
+            window.$ = window.jQuery = require('jquery');
+        }
+
+        if (typeof $().emulateTransitionEnd != 'function') {
+            require('bootstrap-sass');
+        }
+
+        if (typeof $().daterangepicker != 'function') {
+            require('bootstrap-daterangepicker');
+        }
+    } catch (e) {
+        //TODO
+    }
+}
 require('./dist/js/angular-bsdaterangepicker.js');
 module.exports = 'angular-bsdaterangepicker';
